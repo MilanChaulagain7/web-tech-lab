601ea7262a398450c665bb1067861fc3051aba83
Milan Chaulagain <mcgregormilan@gmail.com>
2024-05-29 08:07:02 +0545

    Add files via upload

---
+++ CSS-pseudo class/webtech/json.js
@@ -0,0 +1,4 @@
+movie = '{"name":"Kabhi khusi kabi gam","genre":"emotional","cast":["Bigb","Srk","rhtik"]}';
+console.log(movie);
+a=JSON.parse(movie)
+console.log(a)
---
+++ CSS-pseudo class/webtech/lab.js
@@ -0,0 +1,3 @@
+document.cookie="item=Book,copy;expires=wed,22 NOV 2023 12:00:00 UTC";
+let x = document.cookie;
+alert(x);
